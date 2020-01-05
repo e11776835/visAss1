07009df069d56a5d6f4ef1e302b65b1504ca68f8
Florian Obermayr <e11776835@student.tuwien.ac.at>
2020-01-05 17:26:30 +0100

    alpha-compositing + transferfunction! (still not sure if this is correct)

--- src/shader/raycasting.glsl
+++ src/shader/raycasting.glsl
@@ -37,23 +37,26 @@ void main()
 	
 	switch(renderingMode)
 	{
+		// --------------------------------------------
 		case 0: //render front faces
 		{
 			fragColor = vec4(frontTex, 1);
 			break;
 		}
 		
+		// --------------------------------------------
 		case 1: //render back faces
 		{
 			fragColor = vec4(backTex, 1);
 			break;
 		}
 		
+		// --------------------------------------------
 		case 2: //render volume (MIP)
 		{
 			float max = 0.0;
 
-			for(float len = length(reverse_ray); len > 0.0; len -= stepRate) {
+			for(float len = length(reverse_ray); len > 0; len -= stepRate) {
 				float src = (texture(volume, frontTex)).r;
 
 				if(src > max) {
@@ -66,8 +69,58 @@ void main()
 			fragColor = vec4(max, max, max, 1);
 			break;
 		}
+
+		// --------------------------------------------
 		case 3: //render volume (Alpha-Compositing)
 		{
+			vec4 color = vec4(0, 0, 0, 0);
+
+			for(float len = length(reverse_ray); len > 0; len -= stepRate) {
+				float src = (texture(volume, frontTex)).r;
+
+				vec3 color_1 = vec3(0, 0, 0);
+				vec3 color_2 = vec3(0, 0, 0);
+				float temp;
+
+				if(src <= 0.1) {
+					temp = 0.0;
+				} else if (src < 0.3) {
+					color_1 = vec3(0, 0, 0);
+					color_2 = vec3(1, 0, 0);
+					temp = 0.002;
+
+				} else if (src < 0.5) {
+					color_1 = vec3(1, 0, 0);
+					color_2 = vec3(0, 1, 0);
+					temp = 0.03;
+
+				}  else if (src < 0.6) {
+					color_1 = vec3(0, 0, 1);
+					color_2 = vec3(0, 1, 0);
+					temp = 0.08;
+
+				}  else if (src < 0.8) {
+					color_1 = vec3(0, 1, 0);
+					color_2 = vec3(1, 1, 1);
+					temp = 0.01;
+
+				} else {
+					temp = 0;
+				}
+
+				vec3 colorValue = mix(color_1, color_2, src);
+				vec4 alpha = vec4(colorValue, temp);
+
+				color = alpha.a * alpha + (1 - alpha.a) * color;
+				
+				if (color.a >= 1) {
+					break;
+				} else {
+					frontTex += step;
+				}
+			}
+
+			fragColor = vec4(color.rgb, 1);
 			break;
 		}
 	}
